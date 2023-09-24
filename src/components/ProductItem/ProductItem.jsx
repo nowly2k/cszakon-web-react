5b73b9a1d4680d40933c416b8e7ae9fd477c7ef7
Marsel <nowly2k@mail.ru>
2023-09-24 23:17:41 +0300

    .

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -9,7 +9,7 @@ function ProductItem({ product, className, onAdd }) {
 
   return (
     <div className={"product " + className}>
-      <div className="img">
+      <div className={"img"}>
         <img src={product.image} alt={product.title} />
       </div>
       <div className="title">{product.title}</div>
