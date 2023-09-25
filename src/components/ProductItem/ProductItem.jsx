4d6d16e8b484863bd8b2ad240ed12af551ee6eaa
Marsel <nowly2k@mail.ru>
2023-09-25 12:31:25 +0300

    ю

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -8,20 +8,22 @@ function ProductItem({ product, className, onAdd }) {
   };
 
   return (
-    <div className={"product " + className}>
-      <div className={"img"}>
-        <img src={product.image} alt={product.title} />
+    <div className="product-container">
+      <div className={"product " + className}>
+        <div className={"img"}>
+          <img src={product.image} alt={product.title} />
+        </div>
+        <div className="title">{product.title}</div>
+        <div className="description">{product.description}</div>
+        <div className="price">
+          <span>
+            Стоимость: <b>{product.price}</b>
+          </span>
+        </div>
+        <Button className={"add-btn"} onClick={onAddHandler}>
+          Добавить в корзину
+        </Button>
       </div>
-      <div className="title">{product.title}</div>
-      <div className="description">{product.description}</div>
-      <div className="price">
-        <span>
-          Стоимость: <b>{product.price}</b>
-        </span>
-      </div>
-      <Button className={"add-btn"} onClick={onAddHandler}>
-        Добавить в корзину
-      </Button>
     </div>
   );
 }
